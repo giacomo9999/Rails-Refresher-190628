356ecd008861cdbccbbd1e8531fd6029a1b1830e
James Gary <gmarchesi888@gmail.com>
2019-06-29 13:37:26 -0400

    Added UI functionality.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,13 @@ class ArticlesController < ApplicationController
 
   def create
     puts "***** New article parameters received"
-    # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
   end
 
   def show
