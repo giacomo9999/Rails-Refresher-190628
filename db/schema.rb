0117a3c114197fa958694951d1fbbc237609df3a
James Gary <gmarchesi888@gmail.com>
2019-07-08 15:26:30 -0400

    Added secure password functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_194545) do
+ActiveRecord::Schema.define(version: 2019_07_08_185625) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_07_05_194545) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
